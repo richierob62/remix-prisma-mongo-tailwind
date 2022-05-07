5530d5a256b657efbce7c9a8743b408cb14be1c7
richierob62 <richierob62@gmail.com>
2022-05-07 16:56:51 -0400

    confgured tailwind and prisma

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="h-screen w-full bg-slate-600">
+      <h1 className="font-bold text-5xl text-blue-300">Welcome to Remix</h1>
+    </div>
+  );
+}
